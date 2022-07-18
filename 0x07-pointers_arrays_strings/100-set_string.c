e9689761f441a5bf00be93816d7d567cc28e0cec
Mskithao <patriciakithao@gmail.com>
2022-07-18 12:52:04 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function
+ * @s:  pointer to the source addres
+ * @to: target address
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+		*s = to;
+}
