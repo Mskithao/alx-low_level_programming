c5bfbbc3bfa9376f7aade3db58c2403be08208d0
Mskithao <patriciakithao@gmail.com>
2022-10-19 04:01:34 -0700

    a thing like abslute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @t: computes the absolute value of an intege
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
