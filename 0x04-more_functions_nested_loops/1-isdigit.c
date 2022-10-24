8918e702bebbb67efcd35f0e8f9c80fe6dbfe979
Mskithao <patriciakithao@gmail.com>
2022-10-24 10:27:13 -0700

    must be a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** _isdigit - checks if integer is digit
+ * @c: integer to check
+ * Return: 1 if c is digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+		
