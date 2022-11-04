a7a3136d3aa471ace59634d312bd12c6afae4522
Mskithao <patriciakithao@gmail.com>
2022-11-04 03:10:18 -0700

    first recursion commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints recursion and a new line
+ * *s: pointer to the string to be printed
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+
+}
