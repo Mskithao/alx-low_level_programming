06e309c1007075b6e925c70cd842cbe27f0a3af0
Mskithao <patriciakithao@gmail.com>
2022-11-08 02:56:59 -0800

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
