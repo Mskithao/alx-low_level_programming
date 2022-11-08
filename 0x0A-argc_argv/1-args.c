485ddef93390029376fcb8a046bcf2fc2ecd5c8d
Mskithao <patriciakithao@gmail.com>
2022-11-08 03:19:09 -0800

    hot fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,16 @@
  * Return: 0
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
 
-	Return 0;
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++);
+		printf("%d\n", i - 1);
+	}
+	return 0;
 }
