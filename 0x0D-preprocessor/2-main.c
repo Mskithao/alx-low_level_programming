1330771b2149e34628382dbf31571c25f10a5821
Mskithao <patriciakithao@gmail.com>
2022-11-16 02:04:42 -0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
