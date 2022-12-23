11a5f4cbd9c08da5ec785b4f0272aa96c72b657d
Mskithao <patriciakithao@gmail.com>
2022-12-23 11:59:00 -0800

    a comma makes a diff

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,12 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar('0' + i);
-		putchar(',');
-		putchar (' ');
+		putchar ('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar (' ');
+		}
 	}
 	putchar('\n');
 	return (0);
