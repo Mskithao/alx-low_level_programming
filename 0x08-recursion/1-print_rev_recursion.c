b911a25352b83d334e2e4e40133555f8996ac1d9
Mskithao <patriciakithao@gmail.com>
2023-01-05 04:17:09 -0800

    reverse a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
+
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
