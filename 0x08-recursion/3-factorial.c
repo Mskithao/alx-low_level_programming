2ba8fae08a417bd17930f8a18395a0869dc94a3d
Mskithao <patriciakithao@gmail.com>
2023-01-05 05:48:22 -0800

    fix factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,15 +4,15 @@
  * factorial - factorial function
  * @n: interger
  *
- * return: 0 upon success
+ * Return: -1 if n is less than 0, 1 if n is 0, and 0 upon success
  */
 
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n > 0)
-		return (n * factorial(n - 1));
-	return (1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
 
