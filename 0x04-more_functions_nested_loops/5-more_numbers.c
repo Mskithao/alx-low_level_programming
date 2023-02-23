18934c30d4ff7d2cf2a5ad937a750b3c7b530b31
Mskithao <patriciakithao@gmail.com>
2023-02-23 12:48:54 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * more_numbers - function to print numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			putchar ('j');
+			j++;
+		}
+	putchar ('\n');
+	i++;
+	}
+}
